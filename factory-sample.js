85d68d8941bb818ee49c5ec9cd63692b76ad8100
Raphaël <ra-phi64@hotmail.fr>
2021-06-14 18:09:56 +0200

    index set

--- factory-sample.js
+++ factory-sample.js
@@ -2,23 +2,48 @@
 // DOM Elements
 const refrshBtn = document.querySelectorAll(".refresh-info");
 // launch modal event
-refrshBtn.forEach((btn) => btn.addEventListener("click", readJson));
+refrshBtn.forEach((btn) => btn.addEventListener("click", indexset));
 
+window.onload = function() {
+  indexset();
+};
 
 function link(id) {
 window.location.href = "photographe.html?"+id;}
 
 
-function readJson () {
-  var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-      }
-    };
-    xhttp.open("GET", "http://localhost/projet-6/FishEyeData.json", true);
-    xhttp.send();
-}
+        function readTextFile(file, callback) {
+            var rawFile = new XMLHttpRequest();
+            rawFile.overrideMimeType("application/json");
+            rawFile.open("GET", file, true);
+            rawFile.onreadystatechange = function() {
+                if (rawFile.readyState === 4 && rawFile.status == "200") {
+                    callback(rawFile.responseText);
+                }
+            }
+            rawFile.send(null);
+        }
+
+
+
+        function indexset() {
+
+                  readTextFile("FishEyeData.json", function(text){
+                    var data = JSON.parse(text);
+                    var dataphoto = data.photographers;
+
+          for (var i = 0; i < dataphoto.length; i++) {
+            var objphotographe = dataphoto[i] ;
+            var finaltag = ""
+                for (var a = 0; a < objphotographe.tags.length; a++) {
+                  var tag = objphotographe.tags[a];
+                  finaltag = finaltag+'<p class="tag">'+tag+'</p>'
+                }
+
+                    document.getElementById("list-photographer").innerHTML +=
+                      '<div class="list-photographer-item"> <a id="'+objphotographe.id+'" onClick="link(this.id)" href="#"><div class="list-photographer-item__img"><img class="list-photographer-item__img__content" src="img/ID/'+objphotographe.portrait+'" alt=""></div><h3 class="list-photographer-item__name" >'+objphotographe.name+'</h3><div class="list-photographer-item__caption"><p class="list-photographer-item__caption__location">'+objphotographe.city+'</p><p class="list-photographer-item__caption__phrase">'+objphotographe.tagline+'</p><p class="list-photographer-item__caption__price">'+objphotographe.price+'$ par jour</p></div><div class="list-photographer-item__tags">'+finaltag+'</div></a></div>';}
+                });  }
+
 
 function Creator() {
     this.createPhotographe = function (type) {
